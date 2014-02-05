670dc98a5fde6b8646c5b5aea4d4a0bc9f78dea5
Ali <ahorowit@stanford.edu>
2014-02-05 09:56:37 -0800

    took out turk id, commeted adult R code

--- aliens_alts_analyses/aliens_alts_adults_expts1a&2a.R
+++ aliens_alts_analyses/aliens_alts_adults_expts1a&2a.R
@@ -6,32 +6,21 @@ library(ggplot2)
 d1 <- read.csv("~/new compy/Stanford/Research/Frank Lab/Conferences/CogSci/CogSci 2014/aliens_alts_analyses/aliens_alts_adults_expts1a&2a.csv")
 
 ##############################################
+
 #Assigning responses to correct or incorrect
-
-
 d1$Answer.trial1[d1$Answer.trial1==""] <- NA
 d1$Answer.trial1 <- factor(d1$Answer.trial1)
 d1$correct <- d1$Input.trial1_correct == d1$Answer.trial1
-d1$Input.trial1_type <- factor(d1$Input.trial1_type)
-d1$Input.trial1_adj <- factor(d1$Input.trial1_adj)
-d1$expt <- factor(d1$expt)
 
+##aggregate correct responses along with adjective type and experiment (1a vs 2a)
+agg.data <- aggregate(d1$correct, list(d1$Input.trial1_adj, d1$expt), FUN=sum, na.rm=T)
 
-##adds list information
-agg.data1 <- aggregate(d1$correct, list(d1$Input.trial1_adj, d1$Input.trial1_type, d1$expt), FUN=sum, na.rm=T)
+##adds length i.e. number of total trials
+agg.data.len <- aggregate(d1$correct, list(d1$Input.trial1_adj, d1$expt), FUN=length)
 
-##adds length i.e. number of trials
-agg.data1.len <- aggregate(d1$correct, list(d1$Input.trial1_adj, d1$Input.trial1_type, d1$expt), FUN=length)
-
-
-
-agg.data1$x <- agg.data1$x 
-agg.data1.len$x <- agg.data1.len$x 
-agg.data <- agg.data1
-
-##renaming variables
-names(agg.data) <- c("adj","contrasts","expt", "count")
-agg.data$total <- agg.data1.len$x
+##rename variables, add length and proportion correct
+names(agg.data) <- c("adj","expt", "count")
+agg.data$total <- agg.data.len$x
 agg.data$prop.corr <- agg.data$count / agg.data$total
 
 ##calculating percent of correct answers
@@ -42,14 +31,15 @@ agg.data$err <- sqrt((agg.data$prop.corr * agg.data$q) / agg.data$total)
 
 
 
-############## ggplot!
+### use ggplot to plot proportion correct by Experiment and trial type
 
+##set up plot aethetics 
 plot.style <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), axis.line = element_line(colour="black",size=.5), axis.ticks = element_line(size=.5),legend.justification=c(1,0), legend.position=c(1,.85),legend.title=element_blank(), axis.title.x = element_text(vjust=-.5), axis.title.y = element_text(angle=90,vjust=0.25))
 
 dodge <- position_dodge(width=0.9) 
 limits <- aes(ymax = prop.corr + err, ymin=prop.corr - err) 
 
-
+## plot proportion correct (y) by experiment (x) and adjective type (bar fills)
 qplot(data = agg.data,
 	x = expt,
 	y = prop.corr,
