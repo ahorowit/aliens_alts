621eec57dd4aa78c9a387f5e1bf30e1acda39089
Ali <ahorowit@stanford.edu>
2014-04-30 17:01:03 -0700

    added reviewer comments in general discussion

--- aliens_alts_analyses/aliens_alts_kids_expt1b.R
+++ aliens_alts_analyses/aliens_alts_kids_expt1b.R
@@ -6,63 +6,55 @@ library(ggplot2)
 d1 <- read.csv("~/new compy/Stanford/Research/Frank Lab/Conferences/CogSci/CogSci 2014/aliens_alts_analyses/aliens_alts_kids_expt1b.csv")
 
 
-
-
-##########################TEST TRIALS#########################
+## reshape data from wide to long form
 library(reshape)
 
+## keeps subject information constant, sort trial shape information
 d2=melt.data.frame(d1,c("Sub_ID","DOB","DOT","Age","condition","agegroup"),c("glorp","tibu","peebo", "zib", "glorp_type", "tibu_type", "peebo_type", "zib_type"))
 
-
-
+## recombine so that data sorted in long form
 d2=melt.data.frame(d1,c("Sub_ID","DOB","DOT","Age","condition", "agegroup"),c(8,11,14,17,6,9,12,15))
 data <- d2[1:392,] 
 data$contrasts <- d2$value[393:784]
+
+##rename columns 
 names(data)[7] <- "correct"
-names(data)[6] <- "alien"
+names(data)[6] <- "shape"
+names(data) <- c("Sub_ID","DOB","DOT","Age","condition", "agegroup", "shape", "correct", "contrasts")
 
-names(data) <- c("Sub_ID","DOB","DOT","Age","condition", "agegroup", "alien", "correct", "contrasts")
-
-
-data$contrasts[data$contrasts=="size "] <- "size"
-data$contrasts <- as.factor(data$contrasts)
+## categorize data for analyses
 data$correct <- data$correct==1
-data$alien <- as.factor(data$alien)
-data$alien <- factor(data$alien, levels = c('glorp', 'peebo', 'tibu', 'zib'))
+data$shape <- factor(data$shape, levels = c('glorp', 'peebo', 'tibu', 'zib'))
 data$Age=as.numeric(data$Age)
-as.factor(data$agegroup)
-
-data$agegroup <- as.factor(data$agegroup)
 data$agegroup <-factor(data$agegroup, levels=c('3.0 - 3.5', '3.5 - 4.0', '4.0 - 4.5', '4.5 - 5.0'))
-str(data)
+
+## aggregate correct responses by contrast type and age group
+agg.data <- aggregate(data$correct, list(data$contrasts, data$agegroup), FUN=sum)
+agg.data.len <- aggregate(data$correct, list(data$contrasts, data$agegroup), FUN=length)
+
+
+## rename colunmns, calculate proportion correct and standard error
+names(agg.data) <- c("contrasts", "agegroup", "count")
+agg.data$total <- agg.data.len$x
+agg.data$prop.corr <- agg.data$count / agg.data$total
+agg.data$q <- 1 - agg.data$prop.corr
+agg.data$err <- sqrt((agg.data$prop.corr * agg.data$q) / agg.data$total)
 
 
 
+############ Plot ###########
 
-agg.data2 <- aggregate(data$correct, list(data$contrasts, data$agegroup), FUN=sum)
-agg.data2.len <- aggregate(data$correct, list(data$contrasts, data$agegroup), FUN=length)
-agg.data2$x <- agg.data2$x 
-agg.data2.len$x <- agg.data2.len$x 
-
-names(agg.data2) <- c("contrasts", "agegroup", "count")
-agg.data2$total <- agg.data2.len$x
-agg.data2$prop.corr <- agg.data2$count / agg.data2$total
-
-agg.data2$q <- 1 - agg.data2$prop.corr
-agg.data2$err <- sqrt((agg.data2$prop.corr * agg.data2$q) / agg.data2$total)
-
-
-
-
+## ggplot aethetics 
 plot.style <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), axis.line = element_line(colour="black",size=.5), axis.ticks = element_line(size=.5),legend.justification=c(1,0), legend.position=c(0.30,.75),legend.title=element_blank(), axis.title.x = element_text(vjust=-.5), axis.title.y = element_text(angle=90,vjust=0.25))
 
-
+## error bars 
 dodge <- position_dodge(width=0.9) 
 limits <- aes(ymax = prop.corr + err, ymin=prop.corr - err) 
 
-
+## relabel age groups for prettier labels
 rename(data$agegroup, c("3.0--3.5", "3.5--4.0", "4.0--4.5", "4.5--5.0"))
 
+## plot proportion correct (y) by age group (x) for each contrast type (bar fill)
 qplot(data = agg.data2,
 	x = agegroup,
 	y = prop.corr,
